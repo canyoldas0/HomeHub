ba68283ac6615dec8c87abd00f9bc58e45003ee4
Can Yoldas <canyoldas1997@gmail.com>
2023-11-06 14:45:00 +0100

    Initial Commit

---
+++ HomeHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomeHub
+//
+//  Created by Can Yoldas on 06/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomeHub/HomeHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeHubApp.swift
+//  HomeHub
+//
+//  Created by Can Yoldas on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct HomeHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
